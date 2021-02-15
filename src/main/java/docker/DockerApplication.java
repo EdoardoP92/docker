7a7e70d38c09b5d0342ea94eb7d7d8b83b38e4e1
e.patti <e.patti.guest@posteitaliane.it>
2021-02-15 17:52:16 +0100

    initial commit

---
+++ src/main/java/docker/DockerApplication.java
@@ -0,0 +1,23 @@
+package docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DockerApplication {
+
+	@RequestMapping("/")
+	public String home(@RequestHeader HttpHeaders headers) {
+		return "---------- request header ----------\n"+headers;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerApplication.class, args);
+	}
+
+}
